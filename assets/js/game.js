6c2d30c549b3ab2c5c8b7d442a45e959624da512
yiladien@hotmail.com <yiladien@hotmail.com>
2022-07-31 03:20:21 -0500

    Added random function

--- assets/js/game.js
+++ assets/js/game.js
@@ -33,14 +33,14 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0 , playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      enemyHealth = Math.max(0,enemyHealth - playerAttack);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -58,7 +58,8 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumer(enemyAttack - 3, enemyAttack);
+      playerHealth = Math.max(0,playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -91,7 +92,7 @@ var startGame = function() {
             
             var pickedEnemyName = enemyNames[i];
 
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             // call fight function with enemy-robot
             fight(pickedEnemyName);
             // if player is still alive and we're not at the last enemy in the array
@@ -187,9 +188,19 @@ var shop = function() {
     }
 };
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+};
+
+
+console.log(Math.random())
+
 
 // start the game when the page loads
-startGame();
+// startGame();
 
 // startGame() function
 //  *function that starts the game
